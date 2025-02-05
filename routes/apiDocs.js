02f0f2c16023bb6e6f17eec93abae702f1d0d8e1
Daniel Concha <danielconchaparra@gmail.com>
2025-02-05 20:57:16 -0300

    Ignore api-docs route on swagger documentation

--- routes/apiDocs.js
+++ routes/apiDocs.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger-output.json");
 
-router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(swaggerDocument));
+router.use("/", /*#swagger.ignore = true*/ swaggerUi.serve);
+router.get("/", /*#swagger.ignore = true*/ swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
