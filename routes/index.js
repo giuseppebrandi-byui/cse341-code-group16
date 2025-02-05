aee468d170804ecc964a887ef7fb41e0bd220434
Daniel Concha <danielconchaparra@gmail.com>
2025-02-05 20:52:47 -0300

    Add swagger ui api-docs route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ router.get('/', (req, res) => { res.send('Hello World') });
 router.use('/patients', require('./patients'));
 router.use("/newsletters", require("./newsletters"))
 router.use("/comments", require("./comments"))
+router.use("/api-docs", require("./apiDocs"))
 
 module.exports = router;
