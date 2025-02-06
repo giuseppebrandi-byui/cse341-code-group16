5b2e54d9f96290682b11aeb3a43c96ddc126685a
giuseppebrandi-byui <bra22041@byui.edu>
2025-02-06 13:52:30 +0000

    Implemented validation and error handling for comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,11 +1,11 @@
 const router = require("express").Router()
-
+const { commentValidationRules, validate } = require('../validation/validation-comments.js');
 const commentsController = require("../controllers/comments")
 
 router.get("/", commentsController.getAll);
 router.get("/:id", commentsController.getSingle);
-router.post("/", commentsController.createComment);
-router.put("/:id", commentsController.updateComment);
+router.post("/", commentValidationRules(), validate, commentsController.createComment);
+router.put("/:id", commentValidationRules(), validate, commentsController.updateComment);
 router.delete("/:id", commentsController.deleteComment);
 
 
