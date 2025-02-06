9fbdc2e16726d6cdca427217f2f569462d64f08f
Daniel Concha <danielconchaparra@gmail.com>
2025-02-05 21:56:12 -0300

    Add swagger global tag for patients routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello World') });
 
-router.use('/patients', require('./patients'));
+router.use('/patients', require('./patients') /*#swagger.tags=['Patients']*/ );
 router.use("/newsletters", require("./newsletters") /*#swagger.tags=['Newsletters']*/ )
 router.use("/comments", require("./comments") /*#swagger.tags=['Comments']*/ )
 router.use("/api-docs", require("./apiDocs"))
