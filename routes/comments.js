33a27631c4e7a7499559067cfe82ef59b2f32c3d
giuseppebrandi-byui <bra22041@byui.edu>
2025-02-10 22:42:58 +0000

    Add Authentication for newsletters and comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,13 @@
 const router = require("express").Router()
 const { commentValidationRules, validate } = require('../validation/validation-comments.js');
-const commentsController = require("../controllers/comments")
+const commentsController = require("../controllers/comments");
+const { isAuthenticated } = require('../auth/authenticate');
 
 router.get("/", commentsController.getAll);
 router.get("/:id", commentsController.getSingle);
-router.post("/", commentValidationRules(), validate, commentsController.createComment);
-router.put("/:id", commentValidationRules(), validate, commentsController.updateComment);
-router.delete("/:id", commentsController.deleteComment);
+router.post("/", isAuthenticated, commentValidationRules(), validate, commentsController.createComment);
+router.put("/:id", isAuthenticated, commentValidationRules(), validate, commentsController.updateComment);
+router.delete("/:id", isAuthenticated, commentsController.deleteComment);
 
 
 module.exports = router
--- routes/newsletters.js
+++ routes/newsletters.js
@@ -1,12 +1,14 @@
+
 const router = require("express").Router()
 const { newsletterValidationRules, validate } = require('../validation/validation-newsletters.js');
-const newsletterController = require("../controllers/newsletters")
+const newsletterController = require("../controllers/newsletters");
+const { isAuthenticated } = require('../auth/authenticate');
 
 router.get("/", newsletterController.getAll);
 router.get("/:id", newsletterController.getSingle);
-router.post("/", newsletterValidationRules(), validate, newsletterController.createNewsLetter)
-router.put("/:id", newsletterValidationRules(), validate, newsletterController.updateNewsLetter)
-router.delete("/:id", newsletterController.deleteNewsLetter)
+router.post("/", isAuthenticated, newsletterValidationRules(), validate, newsletterController.createNewsLetter)
+router.put("/:id", isAuthenticated, newsletterValidationRules(), validate, newsletterController.updateNewsLetter)
+router.delete("/:id", isAuthenticated, newsletterController.deleteNewsLetter)
 
 
 module.exports = router
